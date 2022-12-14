aebf9acf47849943061d378bf2768542823ab10b
mohsenvafaei <vafaei.mohsen60@gmail.com>
2022-12-14 19:11:37 +0330

    favorite

--- lib/ui/screens/Favorite/favorite.dart
+++ lib/ui/screens/Favorite/favorite.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:local_notification_flutter_project/ui/data/ClassInfo/favorit_manager.dart';
+import 'package:local_notification_flutter_project/ui/data/ClassInfo/product.dart';
 
 class FavoriteList extends StatelessWidget {
   const FavoriteList({key});
@@ -13,23 +16,30 @@ class FavoriteList extends StatelessWidget {
       ),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Container(
-            width: size.width,
-            height: size.height,
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  width: size.width,
-                  height: size.height,
-                  child: Column(
-                    children: [
-                      Text('data'),
-                    ],
-                  ),
-                );
-              },
-            ),
+          child: ValueListenableBuilder<Box<ProductEntity>>(
+            valueListenable: favoritmanager.listenable,
+            builder: (context, box, child) {
+              final products = box.values.toList();
+              return Container(
+                width: size.width,
+                height: size.height,
+                child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    final data = products[index];
+                    return Container(
+                      width: size.width,
+                      height: size.height,
+                      child: Column(
+                        children: [
+                          Text('data'),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
           ),
         ),
       ),
