e0df753b4ae0f740651b98a502a018309e64a5ba
vafaei.mohsen1362 <96882496+Mohsenvafaei1362@users.noreply.github.com>
2022-12-20 07:55:54 +0330

    save

--- lib/ui/screens/Search/search.dart
+++ lib/ui/screens/Search/search.dart
@@ -27,10 +27,17 @@ class _SearchState extends State<Search> {
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             color: Colors.white,
-            border: Border.all(
-              color: Colors.black12,
-              width: 1,
-            )),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 5,
+              )
+            ]
+            // border: Border.all(
+            //   color: Colors.black12,
+            //   width: 1,
+            // ),
+            ),
         child: Row(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
